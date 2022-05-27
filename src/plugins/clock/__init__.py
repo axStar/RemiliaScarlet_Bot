07011a876611b8f54882a90bb197b886363f62b5
Aa <2272613209@qq.com>
2022-05-28 00:02:25 +0800

    Earth Clock

--- src/plugins/clock/__init__.py
+++ src/plugins/clock/__init__.py
@@ -4,6 +4,7 @@ from nonebot.adapters.onebot.v11 import Event, Bot, MessageSegment
 
 
 eidolon = nonebot.on_command("平原")
+earth = nonebot.on_command("地球")
 
 
 
@@ -16,3 +17,9 @@ async def _(bot: Bot, event: Event):
         await eidolon.finish(MessageSegment.image(image))
     else:
         await eidolon.finish("出现未知错误，请稍后再试。")
+
+
+@earth.handle()
+async def _(bot:Bot, event: Event):
+    image = wfClock.earth()
+    await earth.finish(MessageSegment.image(image))
--- src/plugins/clock/data_source.py
+++ src/plugins/clock/data_source.py
@@ -26,34 +26,50 @@ class wfClock():
             else:
                 worldState = worldState.json()
 
-            for item in worldState["SyndicateMissions"]:
-                if item["Tag"] == "CetusSyndicate":
-                    jobStart = arrow.get(
-                        (int(item["Activation"]["$date"]["$numberLong"])) / 1000, tzinfo="Asia/Shanghai")
-                    jobEnd = arrow.get(
-                        (int(item["Expiry"]["$date"]["$numberLong"])) / 1000, tzinfo="Asia/Shanghai")
-                    break
+        for item in worldState["SyndicateMissions"]:
+            if item["Tag"] == "CetusSyndicate":
+                jobStart = arrow.get(
+                    (int(item["Activation"]["$date"]["$numberLong"])) / 1000, tzinfo="Asia/Shanghai")
+                jobEnd = arrow.get(
+                    (int(item["Expiry"]["$date"]["$numberLong"])) / 1000, tzinfo="Asia/Shanghai")
+                break
 
-            toNight = jobStart.shift(minutes=100)
-            isDay = arrow.now() < toNight
-            mShift = 0 if isDay else 150
-            toNext = toNight if isDay else jobEnd
-            nightList = []
-            for _ in range(5):
-                nightList.append(
-                    toNight.shift(minutes=mShift).format("HH:mm | hh:mm A")
-                )
-                mShift += 150
+        toNight = jobStart.shift(minutes=100)
+        isDay = arrow.now() < toNight
+        mShift = 0 if isDay else 150
+        toNext = toNight if isDay else jobEnd
+        nightList = []
+        for _ in range(5):
+            nightList.append(
+                toNight.shift(minutes=mShift).format("HH:mm | hh:mm A")
+            )
+            mShift += 150
 
-            stateStr = f"当前状态：{'白天' if isDay else '黑夜'}\n\n距离切换：{toNext.humanize(locale='zh-cn', only_distance=True, granularity=['hour', 'minute'])}"
-            nightStr = "\n\n".join(nightList)
-            timeImage = BytesIO()
-            img = Image.open("./src/plugins/clock/data/背景.png")
-            draw = ImageDraw.ImageDraw(img)
-            tfont = ImageFont.truetype("./src/plugins/clock/data/font1.ttf", 42)
-            textW, textH = draw.textsize(stateStr, tfont)
-            draw.text(((800-textW)/2, (368-textH)/2), stateStr, font=tfont)
-            textW, textH = draw.textsize(nightStr, tfont)
-            draw.text(((800-textW)/2, (788-textH)/2 + 368), nightStr, font=tfont)
-            img.save(timeImage, "png")
-            return timeImage
+        stateStr = f"当前状态：{'白天' if isDay else '夜晚'}\n\n距离切换：{toNext.humanize(locale='zh-cn', only_distance=True, granularity=['hour', 'minute'])}"
+        nightStr = "\n\n".join(nightList)
+        timeImage = BytesIO()
+        img = Image.open("./src/plugins/clock/data/背景.png")
+        draw = ImageDraw.ImageDraw(img)
+        tfont = ImageFont.truetype("./src/plugins/clock/data/font1.ttf", 42)
+        textW, textH = draw.textsize(stateStr, tfont)
+        draw.text(((800-textW)/2, (368-textH)/2), stateStr, font=tfont)
+        textW, textH = draw.textsize(nightStr, tfont)
+        draw.text(((800-textW)/2, (788-textH)/2 + 368), nightStr, font=tfont)
+        img.save(timeImage, "png")
+        return timeImage
+
+    @classmethod
+    def earth(cls) -> BytesIO:
+        now = arrow.now().int_timestamp % 28800
+        isDay = now < 14400
+        leftSeconds = 14400 - (now % 14400)
+        toNext = arrow.now().shift(seconds=leftSeconds)
+        earthState = "{}\n\n距离结束：{}".format("白天" if isDay else '夜晚', toNext.humanize(locale='zh-cn', only_distance=True, granularity=['hour', 'minute']))
+        earthImage =BytesIO()
+        img = Image.open("./src/plugins/clock/data/地球背景.png")
+        draw = ImageDraw.ImageDraw(img)
+        tfont = ImageFont.truetype("./src/plugins/clock/data/font1.ttf", 32)
+        textW, textH = draw.textsize(earthState, tfont)
+        draw.text(((400-textW)/2, (400-textH)/2), earthState, font=tfont, align="center")
+        img.save(earthImage, "png")
+        return earthImage
