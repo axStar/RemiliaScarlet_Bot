f34b405f5f759cc46efdc7077c5b1617b244f187
Aa <2272613209@qq.com>
2022-05-28 11:23:44 +0800

    魔胎之境时间查询

--- src/plugins/clock/__init__.py
+++ src/plugins/clock/__init__.py
@@ -6,7 +6,7 @@ from nonebot.adapters.onebot.v11 import Event, Bot, MessageSegment
 eidolon = nonebot.on_command("平原")
 earth = nonebot.on_command("地球")
 vallis = nonebot.on_command("金星")
-
+cambion = nonebot.on_command("火卫二")
 
 @eidolon.handle()
 async def _(bot: Bot, event: Event):
@@ -27,3 +27,11 @@ async def _(bot: Bot, event: Event):
 async def _(bot: Bot, event: Event):
     image = wfClock.vallis()
     await vallis.finish(MessageSegment.image(image))
+
+@cambion.handle()
+async def _(bot: Bot, event: Event):
+    image = await wfClock.cambion()
+    if image:
+        await eidolon.finish(MessageSegment.image(image))
+    else:
+        await eidolon.finish("出现未知错误，请稍后再试。")
--- src/plugins/clock/data_source.py
+++ src/plugins/clock/data_source.py
@@ -12,8 +12,8 @@ class wfClock():
     def __init__(self) -> None:
         pass
 
-    @classmethod
-    async def eidolon(cls) -> Union[BytesIO, None]:
+    @staticmethod
+    async def eidolon() -> Union[BytesIO, None]:
         """获取夜灵平野的状态。"""
 
         async with httpx.AsyncClient() as client:
@@ -45,14 +45,15 @@ class wfClock():
             )
             mShift += 150
 
-        stateStr = f"当前状态：{'白天' if isDay else '夜晚'}\n\n距离切换：{toNext.humanize(locale='zh-cn', only_distance=True, granularity=['hour', 'minute'])}"
+        stateStr = f"当前状态：{'白天' if isDay else '夜晚'}\n\n距离切换：{toNext.humanize(locale='zh-cn', only_distance=True, granularity=['hour', 'minute', 'second'])}"
         nightStr = "\n\n".join(nightList)
         timeImage = BytesIO()
         img = Image.open("./src/plugins/clock/data/夜灵背景.png")
         draw = ImageDraw.ImageDraw(img)
-        tfont = ImageFont.truetype("./src/plugins/clock/data/font1.ttf", 42)
+        tfont = ImageFont.truetype("./src/plugins/clock/data/font1.ttf", 55)
         textW, textH = draw.textsize(stateStr, tfont)
-        draw.text(((800-textW)/2, (368-textH)/2), stateStr, font=tfont)
+        draw.text(((800-textW)/2, (368-textH)/2),
+                  stateStr, font=tfont, align="center")
         textW, textH = draw.textsize(nightStr, tfont)
         draw.text(((800-textW)/2, (788-textH)/2 + 368), nightStr, font=tfont)
         img.save(timeImage, "png")
@@ -64,12 +65,12 @@ class wfClock():
         isDay = now < 14400
         leftSeconds = 14400 - (now % 14400)
         toNext = arrow.now().shift(seconds=leftSeconds)
-        earthState = "{}\n\n距离结束：{}".format("白天" if isDay else '夜晚', toNext.humanize(
-            locale='zh-cn', only_distance=True, granularity=['hour', 'minute']))
+        earthState = "当前状态：{}\n\n距离结束：{}".format("白天" if isDay else '夜晚', toNext.humanize(
+            locale='zh-cn', only_distance=True, granularity=['hour', 'minute', 'second']))
         earthImage = BytesIO()
         img = Image.open("./src/plugins/clock/data/时间背景.png")
         draw = ImageDraw.ImageDraw(img)
-        tfont = ImageFont.truetype("./src/plugins/clock/data/font1.ttf", 32)
+        tfont = ImageFont.truetype("./src/plugins/clock/data/font1.ttf", 28)
         textW, textH = draw.textsize(earthState, tfont)
         draw.text(((400-textW)/2, (400-textH)/2),
                   earthState, font=tfont, align="center")
@@ -80,7 +81,7 @@ class wfClock():
     def vallis() -> BytesIO:
         start = arrow.get("2018-11-10 08:13:48")
         loop = 1600
-        cold = 400
+        cold = 1200
         sinceLast = (arrow.now().int_timestamp - start.int_timestamp) % loop
         toNextFull = loop - sinceLast
         state = "寒冷"
@@ -90,14 +91,47 @@ class wfClock():
         else:
             toNextMinor = toNextFull
 
-        stateStr = "{}\n\n距离结束：{}".format(state, arrow.now().shift(seconds=toNextMinor).humanize(
-            locale='zh-cn', only_distance=True, granularity=['hour', 'minute']))
+        stateStr = "当前状态：{}\n\n距离结束：{}".format(state, arrow.now().shift(seconds=toNextMinor).humanize(
+            locale='zh-cn', only_distance=True, granularity=['hour', 'minute', 'second']))
         vallisImage = BytesIO()
         img = Image.open("./src/plugins/clock/data/时间背景.png")
         draw = ImageDraw.ImageDraw(img)
-        tfont = ImageFont.truetype("./src/plugins/clock/data/font1.ttf", 32)
+        tfont = ImageFont.truetype("./src/plugins/clock/data/font1.ttf", 28)
         textW, textH = draw.textsize(stateStr, tfont)
         draw.text(((400-textW)/2, (400-textH)/2),
                   stateStr, font=tfont, align="center")
         img.save(vallisImage, "png")
         return vallisImage
+
+    async def cambion() -> Union[BytesIO, None]:
+        async with httpx.AsyncClient() as client:
+            try:
+                worldState = await client.get("http://content.warframe.com/dynamic/worldState.php")
+                if worldState.status_code != 200:
+                    raise Exception(f"错误代码：{worldState.status_code}")
+            except:
+                return None
+            else:
+                worldState = worldState.json()
+
+        for item in worldState["SyndicateMissions"]:
+            if item["Tag"] == "CetusSyndicate":
+                jobStart = arrow.get(
+                    (int(item["Activation"]["$date"]["$numberLong"])) / 1000, tzinfo="Asia/Shanghai")
+                jobEnd = arrow.get(
+                    (int(item["Expiry"]["$date"]["$numberLong"])) / 1000, tzinfo="Asia/Shanghai")
+                break
+
+        toNight = jobStart.shift(minutes=100)
+        isDay = arrow.now() < toNight
+        toNext = toNight if isDay else jobEnd
+        stateStr = f"当前状态：{'Fass' if isDay else 'Vome'}\n\n距离结束：{toNext.humanize(locale='zh-cn', only_distance=True, granularity=['hour', 'minute', 'second'])}"
+        timeImage = BytesIO()
+        img = Image.open("./src/plugins/clock/data/时间背景.png")
+        draw = ImageDraw.ImageDraw(img)
+        tfont = ImageFont.truetype("./src/plugins/clock/data/font1.ttf", 28)
+        textW, textH = draw.textsize(stateStr, tfont)
+        draw.text(((400-textW)/2, (400-textH)/2),
+                  stateStr, font=tfont, align="center")
+        img.save(timeImage, "png")
+        return timeImage
